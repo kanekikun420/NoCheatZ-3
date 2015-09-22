378b651df9277ba777a311b22bd01fb129e66a63
Le Padellec Sylvain <le.padellec.sylvain@gmail.com>
2015-09-22 09:47:06 +0200

    New portable eventmanager

---
+++ server-plugin/Code/Misc/EventManagerWrap.h
@@ -0,0 +1,35 @@
+#include "igameevents.h"
+
+#ifndef EV_MANAGER_WRAP
+#define EV_MANAGER_WRAP
+
+#ifndef EVENT_DEBUG_ID_INIT
+#	define EVENT_DEBUG_ID_INIT 42
+#endif
+#ifndef EVENT_DEBUG_ID_SHUTDOWN
+#	define EVENT_DEBUG_ID_SHUTDOWN 13
+#endif
+
+class EventManagerWrap : public IGameEventListener2
+{
+	EventManagerWrap()
+	{
+		m_debugid = EVENT_DEBUG_ID_INIT;
+	};
+	virtual ~EventManagerWrap()
+	{
+		m_debugid = EVENT_DEBUG_ID_SHUTDOWN;
+	};
+
+	virtual void FireGameEvent(IGameEvent* ev) = 0;
+
+	virtual int GetEventDebugID() const
+	{
+		return m_debugid;
+	};
+
+private:
+	int m_debugid;
+};
+
+#endif
